8ab28df2e76c6ef3b07740006ebfb8ce4bc1ab23
tayyib57 <61462239+tayyib57@users.noreply.github.com>
2021-04-26 20:35:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const addRed = document.querySelector(".add-1");
+const addGreen = document.querySelector(".add-2");
+const addYellow = document.querySelector(".add-3");
+
+
+addRed.addEventListener("click", function() {
+    
+    monTest1.afficheTest();
+});
+
+addGreen.addEventListener("click", function() {
+    monTest2.afficheTest();
+   
+});
+
+addYellow.addEventListener("click", function() {
+    monTest3.afficheTest();
+   
+});
